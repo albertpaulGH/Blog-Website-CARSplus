c755ccbfa8ce32dffff1cf209b92705a33327cd2
albertpaulGH <albertpaulwork@gmail.com>
2024-08-19 22:57:53 +0530

    Use env variables and create Procfile

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blog.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URI", "sqlite:///blog.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -268,4 +268,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False)
